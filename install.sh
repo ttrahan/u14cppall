ca882969c693ad863c55273ba10dcae2c5083deb
mrinalam <mrinal.cena@gmail.com>
2017-02-07 13:10:00 +0000

    changed the gcc version

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ sudo apt-get clean
 sudo apt-get update
 echo "======================== Cleaned apt-get ======================"
 
-echo "======================= Installing gcc 5 ======================"
+echo "======================= Installing gcc 6 ======================"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install gcc-5
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
+sudo apt-get install gcc-6 g++-6
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 50 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 gcc --version
-echo "================== Successfully Installed gcc 5 ==============="
+echo "================== Successfully Installed gcc 6 ==============="
 
 echo "==================== Installing clang 3.9.0 ==================="
 sudo wget http://llvm.org/releases/3.9.0/clang+llvm-3.9.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
---
+++ test/_cppall.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash -e
+
+echo "==================== Testing gcc ================"
+gcc --version
+echo "==================== Tested successfully ==========="
+
+echo "==================== Testing clang ================"
+clang --version
+echo "==================== Tested successfully ==========="
