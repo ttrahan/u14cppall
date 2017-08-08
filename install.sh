d8a9e0caa26eb89b4f463b91b6662a3ec89b7510
Niranjan Godbole <niranjan8192@gmail.com>
2017-08-08 07:08:38 +0000

    lock down gcc version to 7.1

--- install.sh
+++ install.sh
@@ -11,16 +11,19 @@ echo "======================== Cleaned apt-get ======================"
 echo "======================= Installing gcc 7.1 ======================"
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install gcc-7 g++-7
+apt-get install -y \
+  gcc-7=7.1.0-5ubuntu2~14.04 \
+  g++-7=7.1.0-5ubuntu2~14.04
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 50 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 gcc --version
 echo "================== Successfully Installed gcc 7.1 ==============="
 
-echo "==================== Installing clang 4.0.0 ==================="
-wget -nv http://llvm.org/releases/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-tar xf clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
-cd clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04
+CLANG_VERSION=4.0.0
+echo "==================== Installing clang $CLANG_VERSION ==================="
+wget -nv http://llvm.org/releases/"$CLANG_VERSION"/clang+llvm-"$CLANG_VERSION"-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+tar xf clang+llvm-"$CLANG_VERSION"-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+cd clang+llvm-"$CLANG_VERSION"-x86_64-linux-gnu-ubuntu-14.04
 cp -R * /usr/local/
 cd ../
 clang --version
-echo "=============== Successfully Installed clang 4.0.0 ============"
+echo "=============== Successfully Installed clang $CLANG_VERSION ============"
